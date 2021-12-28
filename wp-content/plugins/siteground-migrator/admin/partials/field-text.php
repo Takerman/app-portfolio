9e9eb946d708df0f8d0835e95d19d12b21c93211
Tanyo Ivanov <tivanov@takerman.net>
2021-12-28 12:00:49 +0200

    Remove siteground migrator

--- wp-content/plugins/siteground-migrator/admin/partials/field-text.php
+++
@@ -1,9 +0,0 @@
-<input
-	name="<?php echo esc_attr( $this->get_id() ); ?>"
-	id="<?php echo esc_attr( $this->get_id() ); ?>"
-	type="text"
-	value="<?php echo esc_attr( $this->get_value() ); ?>"
-	class="<?php echo esc_attr( $this->get_class_names() ) ?>"
-	<?php echo $this->add_pattern() ?>
-	<?php echo $this->is_required() ?>
-/>
