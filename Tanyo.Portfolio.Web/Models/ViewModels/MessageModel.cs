8b710b1f8f44b6b0e6b6b973f5d17a3f0b3d4a4d
Tanyo Ivanov <tivanov@takerman.net>
2022-11-27 23:53:40 +0200

    Model validation for the contact form

--- Tanyo.Portfolio.Web/Models/ViewModels/MessageModel.cs
+++ Tanyo.Portfolio.Web/Models/ViewModels/MessageModel.cs
@@ -1,13 +1,22 @@
-﻿namespace Tanyo.Portfolio.Web.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tanyo.Portfolio.Web.Models.ViewModels
 {
     public class MessageModel
     {
+
+        [StringLength(300)]
         public string Subject { get; set; }
 
+        [StringLength(600)]
         public string Message { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(300)]
         public string Name { get; set; }
     }
 }
